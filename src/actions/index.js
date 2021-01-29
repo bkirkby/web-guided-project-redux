bd78d3b2b6b98683299235deccddd03fe19e6ed9
kirkby <kirkby@neighbor.com>
2021-01-28 18:19:08 -0700

    progress

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const TOGGLE_EDITING = 'TOGGLE_EDITING';
 export const UPDATE_TITLE = 'UPDATE_TITLE';
 export const ADD_NEW_MEMBER = 'ADD_NEW_MEMBER';
+export const TOGGLE_DRAGON_STATUS = 'TOGGLE_DRAGON_STATUS';
 
 export const toggleEditing = () => {
   return {
@@ -19,5 +20,12 @@ export const addNewMember = (newMemberName) => {
   return {
     type: ADD_NEW_MEMBER,
     payload: newMemberName
+  }
+}
+
+export const toggleDragonStatus = (memberName) => {
+  return {
+    type: TOGGLE_DRAGON_STATUS,
+    payload: memberName
   }
 }
