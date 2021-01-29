616c68e58cead5346e5b8aacd2c8f85686c8c59f
kirkby <kirkby@neighbor.com>
2021-01-28 21:08:05 -0700

    progress

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,3 +2,5 @@ import { combineReducers } from 'redux';
 
 import { titleReducer } from './titleReducer';
 import { membersReducer } from './membersReducer';
+
+export default combineReducers({ titleReducer, membersReducer });
