3ce0d7d937701a0ecdcc0d31024c9321572ddf63
kirkby <kirkby@neighbor.com>
2021-01-28 19:49:20 -0700

    progress

--- src/index.js
+++ src/index.js
@@ -21,4 +21,4 @@ function App() {
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
