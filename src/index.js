d6f476fd9017a6754a3f8b5f3625c8411ed0bde9
kirkby <kirkby@neighbor.com>
2021-01-28 17:39:10 -0700

    progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'react';
 
 import Title from './components/Title';
 import DragonList from './components/DragonList';
 import './styles.css';
+import { titleReducer } from './reducers/titleReducer';
+
+const reduxStore = createStore(/* reducer */);
 
 function App() {
   return (
