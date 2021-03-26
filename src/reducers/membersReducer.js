35aca9eab282f3703bc85b37afdbc929039df0db
kirkby <kirkby@neighbor.com>
2021-03-25 20:59:46 -0600

    progress

---
+++ src/reducers/membersReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  members: [
+    { name: 'Jojo Zhang', dragonStatus: true },
+    { name: 'Brandon Harris', dragonStatus: false }
+  ]
+}
+
+export const membersReducer = (state, action) => {
+  switch (action.type) {
+    default: return state
+  }
+}
