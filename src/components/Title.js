63b04fec638f9ac2539123b52919b55e77357b26
kirkby <kirkby@neighbor.com>
2021-03-25 21:20:43 -0600

    progress

--- src/components/Title.js
+++ src/components/Title.js
@@ -51,8 +51,8 @@ const Title = (props) => {
 
 const mapStateToProps = state => {
   return {
-    editing: state.editing,
-    title: state.title
+    editing: state.title.editing,
+    title: state.title.title
   }
 }
 
