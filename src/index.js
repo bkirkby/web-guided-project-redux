f6d4dfba052159d542a830c5669dbcbfaaf12b39
kirkby <kirkby@neighbor.com>
2021-05-20 19:55:50 -0600

    update progress!!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
 import Title from './components/Title';
 import DragonList from './components/DragonList';
