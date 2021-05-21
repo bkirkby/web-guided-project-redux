d70269d2374fbdb8aee66746860a04eb6fb70834
kirkby <kirkby@neighbor.com>
2021-05-20 21:18:16 -0600

    update progress!!

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const TOGGLE_EDITING = 'TOGGLE_EDITING';
 export const UPDATE_TITLE = 'UPDATE_TITLE';
+export const ADD_MEMBER = 'ADD_MEMBER';
 
 export const toggleEditing = () => {
   return { type: TOGGLE_EDITING }
@@ -9,5 +10,12 @@ export const updateTitle = newTitle => {
   return {
     type: UPDATE_TITLE,
     payload: newTitle
+  }
+}
+
+export const addMember = newMemberName => {
+  return {
+    type: ADD_MEMBER,
+    payload: newMemberName
   }
 }
