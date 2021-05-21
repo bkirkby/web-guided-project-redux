9abecba07229bee1f9759483716a3208a1b5fda3
kirkby <kirkby@neighbor.com>
2021-05-20 21:24:26 -0600

    update progress!!

--- src/reducers/membersReducer.js
+++ src/reducers/membersReducer.js
@@ -14,6 +14,19 @@ export const membersReducer = (state = initialState, action) => {
         ...state,
         members: [...state.members, { name: action.payload, dragonStatus: false }]
       }
+    case TOGGLE_DRAGON_STATUS:
+      return {
+        ...state,
+        members: state.members.map(member => {
+          if (member.name === action.payload) {
+            return {
+              ...member,
+              dragonStatus: !member.dragonStatus
+            }
+          }
+          return member;
+        }
+      }
     default: return state;
   }
 }
