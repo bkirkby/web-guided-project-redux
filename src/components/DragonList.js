2e36d85ea239c7626970a3b5b3fc1c933a4873d3
kirkby <kirkby@neighbor.com>
2021-05-20 21:28:41 -0600

    update progress!!

--- src/components/DragonList.js
+++ src/components/DragonList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { addMember } from '../actions';
+import { addMember, toggleDragonStatus } from '../actions';
 
 class DragonList extends React.Component {
   state = {
